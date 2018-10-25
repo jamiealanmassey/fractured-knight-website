2e2dfec1e5d054b6673072c13629fba9df4bb4fc
= <jamiealanmassey@gmail.com>
2018-10-25 18:19:04 +0100

    Split mongoose models into their own indepedent folder under 'models', also started implmentation for authentication

---
+++ models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  subheading: String,
+  image: {type: String, default: 'img/image-default.jpg'},
+  content: String,
+  created: {type: Date, default: Date.now},
+  author: String,
+  tags: [String]
+});
+
+mongoose.model('Post', postSchema);

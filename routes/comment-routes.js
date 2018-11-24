78dc8405a4c770df04d91e73c3be8822ce1f7426
Jamie Massey <jamiealanmassey@gmail.com>
2018-11-24 20:07:39 +0000

    Comments Working
    Can set comments and number of comments are correctly reflected as so in the all.ejs webpage for each post

--- routes/comment-routes.js
+++ routes/comment-routes.js
@@ -4,6 +4,27 @@ const Post = mongoose.model('Post');
 const express = require('express');
 const router = express.Router();
 
+function validateRemoval(request, response, next) {
+    console.log("passed user id: " + request.params.id);
+    Post.findOne({"_id" : request.params.id})
+        .exec(function(error, post) {
+            if (error) {
+                response.redirect('/posts/' + request.params.id);
+            } else {
+                console.log("comment structure: " + post);
+                console.log("comment user id: " + post.postedBy);
+                console.log(post.comments.some(element => element.postedBy.equals(request.user.comment)));
+
+                if (request.isAuthenticated() && request.user._id.equals(post.postedBy)) {
+                    console.log("Request successfully deleted");
+                    return next();
+                }
+            }
+        });
+
+    response.redirect('/posts/' + request.params._id);
+}
+
 function createComment(request, response) {
     console.log("creating comment...");
     Post.findOneAndUpdate({"_id" : request.params.id},
@@ -26,6 +47,20 @@ function createComment(request, response) {
     });
 }
 
+function deleteComment(request, response) {
+    console.log("deleting comment...");
+    Post.findOneAndRemove({"comments._id" : request.params.comment})
+        .exec(function(error, comment) {
+            if (error) {
+                response.status(500).send(error);
+            } else {
+                console.log('redirecting to /posts/' + request.params.id);
+                response.redirect('/posts/' + request.params.id);
+            }
+        });
+}
+
 router.post('/posts/:id/comments', identify.isUser, createComment);
+//router.delete('/posts/:id/comments/:comment', validateRemoval, deleteComment);
 
 module.exports = router;
